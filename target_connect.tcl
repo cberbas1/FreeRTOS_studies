5b67ca20c697351f137875b71f20125c4ae3346f
Berk Erbas <cberbas@gmail.com>
2025-04-21 21:53:11 +0100

    [xsct_flash_scripts] Add target connection script with target listing functionality

---
+++ target_connect.tcl
@@ -0,0 +1,55 @@
+## @author: Berk Erbas
+## @date: 2023-10-04
+## @description: This script connects to a target.
+
+#procedure to connect to the target
+
+#procedure to list the available targets
+proc list_targets {} {
+    #get the target list
+    set target_list [targets]
+
+    # Split into lines and parse target info
+    set lines [split $target_list "\n"]
+    set target_list [list]
+
+    foreach line $lines {
+        # Extract index, name, and state using regex
+        if {[regexp {^\s*(\d+)\s+(Cortex\S*)\s+(.*)} $line -> index name state]} {
+            lappend parsed_targets [dict create index $index name $name state $state]
+        }
+    }
+    
+    return $parsed_targets
+}
+
+proc print_avilable {available_targets} {
+    puts "\nAvailable processor targets:"
+    puts [format "%-10s %-20s %-15s" "Index" "Name" "State"]  ;# Header with fixed widths
+    puts "----------------------------------------------------------"
+
+    foreach target $available_targets {
+        set index [dict get $target index]
+        set name [dict get $target name]
+        set state [dict get $target state]
+        puts [format "%-10s %-20s %-15s" $index $name $state]
+    }
+}
+
+# Call the procedure to get parsed targets
+connect
+set available_targets [list_targets]
+
+# Print targets
+print_avilable $available_targets
+
+#list the available targets
+#make this a procedure
+
+#print the targets for user
+
+
+#ask user to choose a target
+#if user makes a mistake, ask again
+#make this a procedure
+
